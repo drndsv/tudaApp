b461c1f76961f82b04d8fcf703900f4599f02814
dashk <burckina.anna@yandex.ru>
2025-07-01 10:04:08 +0300

    частично работает страница мероприятий пользователя (нужно доделать когда будет авторизация)

--- src/pages/UserEventsPage.tsx
+++ src/pages/UserEventsPage.tsx
@@ -20,10 +20,12 @@ export default function UserEventsPage() {
   const user = JSON.parse(localStorage.getItem("user") || "{}");
 
   useEffect(() => {
-    if (!user?.id) return;
+    // if (!user?.id) return;
 
-    EventControllerService.getEventsByUserId(user.id)
+    // EventControllerService.getEventsByUserId(user.id)
+    EventControllerService.getEventsByUserId(1)
       .then((response) => {
+        console.log("Ответ от сервера:", response);
         if (!response.error && response.result) {
           setEvents(response.result);
         } else {
